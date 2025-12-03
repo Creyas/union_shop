c7995d353416ff92002c6f8f600d9cb252750329
Josh Aadur <joshaadur@gmail.com>
2025-12-03 19:32:20 +0000

    Update AboutPage content with detailed information about the Union Shop

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -15,7 +15,17 @@ class AboutPage extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(24.0),
               child: SingleChildScrollView(
-                child: Text('Your about content here.'),
+                child: Text("""Welcome to the Union Shop!
+
+We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!
+
+All online purchases are available for delivery or instore collection!
+
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.
+
+Happy shopping!
+
+The Union Shop & Reception Team​​​​​​​​​"""),
               ),
             ),
           ),
