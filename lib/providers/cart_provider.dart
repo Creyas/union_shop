4ea0d612cb44cdbc1e423718f9bda2f208bc7199
Josh Aadur <joshaadur@gmail.com>
2025-12-04 17:06:09 +0000

    Add CartProvider to manage shopping cart functionality

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../models/cart_item.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  int get itemCount => _items.length;
+
+  double get totalPrice {
+    return _items.fold(0, (sum, item) => sum + item.total);
+  }
+
+  void addItem(CartItem item) {
+    final existingIndex = _items.indexWhere(
+      (existingItem) =>
+          existingItem.id == item.id &&
+          existingItem.color == item.color &&
+          existingItem.size == item.size,
+    );
+
+    if (existingIndex >= 0) {
+      _items[existingIndex] = _items[existingIndex].copyWith(
+        quantity: _items[existingIndex].quantity + item.quantity,
+      );
+    } else {
+      _items.add(item);
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String id) {
+    _items.removeWhere((item) => item.id == id);
+    notifyListeners();
+  }
+
+  void updateQuantity(String id, int quantity) {
+    final index = _items.indexWhere((item) => item.id == id);
+    if (index >= 0) {
+      if (quantity <= 0) {
+        _items.removeAt(index);
+      } else {
+        _items[index] = _items[index].copyWith(quantity: quantity);
+      }
+      notifyListeners();
+    }
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
+}
